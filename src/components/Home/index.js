ce426ef68f1f8a4fbefbd088eb56c257432ac19e
Jerson Suaza <jerson.suaza@gmail.com>
2020-04-04 11:57:29 -0500

    refactor profileContext -> store

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,25 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Profile } from './Profile'
 import { ViewCats } from './ViewCats'
 import { AboutCat } from './AboutCat'
-import { ProfileContext } from '../../contexts/ProfileContext'
+import { ProfileContextStore } from '../../contexts/ProfileContext'
 
-export const Home = () => {
-
-    const [ profilePanel, setProfilePanel ] = useState(false)
-
-    useEffect( () =>{
-        const showState = window.innerWidth > 768 //true si se cumple > 768
-        setProfilePanel(showState)
-    }, [])
-    
-    return (
-        <div className="home-page">
-            <ProfileContext.Provider value={{ profilePanel, setProfilePanel }}>
-                <Profile />
-                <ViewCats />
-                <AboutCat />
-            </ProfileContext.Provider>
-        </div>
-    )
-}
+export const Home = () => (
+    <div className="home-page">
+        <ProfileContextStore>
+            <Profile />
+            <ViewCats />
+            <AboutCat />
+        </ProfileContextStore>
+    </div>
+)
--- src/contexts/ProfileContext.js
+++ src/contexts/ProfileContext.js
@@ -1,3 +1,21 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 
-export const ProfileContext = createContext(null)
+const initialState = false
+
+export const ProfileContext = createContext(initialState)
+
+export const ProfileContextStore = ({ children }) => {
+
+    const [ profilePanel, setProfilePanel ] = useState(initialState)
+
+    useEffect( () => {
+        const showState = window.innerWidth > 768 //true si se cumple > 768
+        setProfilePanel(showState)
+    }, [])
+
+    return (
+        <ProfileContext.Provider value={{ profilePanel, setProfilePanel }}>
+            { children }
+        </ProfileContext.Provider>
+    )
+}
